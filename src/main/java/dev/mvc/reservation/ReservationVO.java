62f1949b0a083357cba8a525da07b26f7020116c
JSYANG0126 <didwnstjr23@naver.com>
2023-11-14 12:23:21 +0900

    VO 업데이트 -> 예메 페이지 , 영화관 추천

---
+++ src/main/java/dev/mvc/reservation/ReservationVO.java
@@ -0,0 +1,27 @@
+package dev.mvc.reservation;
+
+public class ReservationVO {
+  private int reservationno;
+  private String tname;
+  private String link;
+  
+  public int getReservationno() {
+    return reservationno;
+  }
+  public void setReservationno(int reservationno) {
+    this.reservationno = reservationno;
+  }
+  public String getTname() {
+    return tname;
+  }
+  public void setTname(String tname) {
+    this.tname = tname;
+  }
+  public String getLink() {
+    return link;
+  }
+  public void setLink(String link) {
+    this.link = link;
+  }
+  
+}
---
+++ src/main/java/dev/mvc/theater/TheaterVO.java
@@ -0,0 +1,49 @@
+package dev.mvc.theater;
+
+public class TheaterVO {
+  private int theaterno;
+  private int memno;
+  private String ttitle;
+  private String tinfo;
+  private String tdate;
+  private String map;
+  
+  public int getTheaterno() {
+    return theaterno;
+  }
+  public void setTheaterno(int theaterno) {
+    this.theaterno = theaterno;
+  }
+  public int getMemno() {
+    return memno;
+  }
+  public void setMemno(int memno) {
+    this.memno = memno;
+  }
+  public String getTtitle() {
+    return ttitle;
+  }
+  public void setTtitle(String ttitle) {
+    this.ttitle = ttitle;
+  }
+  public String getTinfo() {
+    return tinfo;
+  }
+  public void setTinfo(String tinfo) {
+    this.tinfo = tinfo;
+  }
+  public String getTdate() {
+    return tdate;
+  }
+  public void setTdate(String tdate) {
+    this.tdate = tdate;
+  }
+  public String getMap() {
+    return map;
+  }
+  public void setMap(String map) {
+    this.map = map;
+  }
+  
+  
+}
