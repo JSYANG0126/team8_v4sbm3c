70d638e02280cee0161be99682fb7e579c34adda
JSYANG0126 <didwnstjr23@naver.com>
2023-11-14 10:50:38 +0900

    SQL 업데이트 -> 예매 페이지, 영화관 추천

--- src/main/webapp/WEB-INF/doc/영화관 추천/theater.sql
+++ src/main/webapp/WEB-INF/doc/영화관 추천/theater.sql
@@ -8,6 +8,13 @@ CREATE TABLE theater(
   FOREIGN KEY (memno) REFERENCES MEM (memno)
 );
 
+CREATE SEQUENCE THEATER_SEQ
+  START WITH 1         -- 시작 번호
+  INCREMENT BY 1       -- 증가값
+  MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+  CACHE 2              -- 2번은 메모리에서만 계산
+  NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
+
 COMMENT ON TABLE theater is '영화관 추천';
 COMMENT ON COLUMN theater.theaterno is '영화관 번호';
 COMMENT ON COLUMN theater.memno is '회원 번호';
@@ -15,3 +22,22 @@ COMMENT ON COLUMN theater.ttitle is '제목';
 COMMENT ON COLUMN theater.tinfo is '내용';
 COMMENT ON COLUMN theater.tdate is '날짜';
 COMMENT ON COLUMN theater.map is '지도';
+
+INSERT INTO theater(theaterno, memno, ttitle, tinfo, tdate, map) 
+VALUES(theater_seq.nextval, '1', 'abd' , 15);  
+
+-- READ: LIST
+SELECT * FROM theater;
+SELECT theaterno, memno, ttitle, tinfo, tdate, map FROM theater ORDER BY theaterno ASC;
+
+-- READ
+SELECT theaterno, memno, ttitle, tinfo, tdate, map FROM theater WHERE theaterno=1;
+
+-- UPDATE
+UPDATE theater SET ttitle='고길동', tinfo='dvc' WHERE theaterno=1;
+
+
+-- DELETE
+DELETE FROM theater WHERE theaterno=1;
+
+COMMIT;
--- src/main/webapp/WEB-INF/doc/예매페이지/reservation.sql
+++ src/main/webapp/WEB-INF/doc/예매페이지/reservation.sql
@@ -8,3 +8,29 @@ COMMENT ON TABLE reservation is '예매페이지';
 COMMENT ON COLUMN reservation.reservationno is '예매번호';
 COMMENT ON COLUMN reservation.tname is '이름';
 COMMENT ON COLUMN reservation.link is '주소';
+
+CREATE SEQUENCE RESERVATION_SEQ
+  START WITH 1         -- 시작 번호
+  INCREMENT BY 1       -- 증가값
+  MAXVALUE 9999999999  -- 최대값: 9999999999 --> NUMBER(10) 대응
+  CACHE 2              -- 2번은 메모리에서만 계산
+  NOCYCLE;             -- 다시 1부터 생성되는 것을 방지
+  
+INSERT INTO reservation(reservationno, tname, link) 
+VALUES(reservation_seq.nextval, 'CGV', 'www.cgv.com');  
+
+-- READ: LIST
+SELECT * FROM reservation;
+SELECT reservationno, tname, link FROM reservation ORDER BY reservationno ASC;
+
+-- READ
+SELECT reservationno, tname, link FROM reservation WHERE reservationno=1;
+
+-- UPDATE
+UPDATE reservation SET tname='lotte cinema', link='www.lotte cinema' WHERE reservationno=1;
+
+
+-- DELETE
+DELETE FROM reservation WHERE reservationno=1;
+
+COMMIT;
