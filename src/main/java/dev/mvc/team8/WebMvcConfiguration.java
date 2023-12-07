7537102310cabde93c3ba9de7b9bf7a7c7d8515f
alsehdgnl <alsehdgnl8658@daum.net>
2023-12-07 14:42:08 +0900

    커밋용

---
+++ src/main/java/dev/mvc/team8/WebMvcConfiguration.java
@@ -0,0 +1,30 @@
+package dev.mvc.team8;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import dev.mvc.movie.Movie;
+import dev.mvc.tool.Tool;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Windows: path = "C:/kd/deploy/MR_v2sbm3c_blog/mreview/storage";
+        // ▶ file:///C:/kd/deploy/MR_v2sbm3c_blog/mreview/storage
+      
+        // Ubuntu: path = "/home/ubuntu/deploy/MR_v2sbm3c_blog/mreview/storage";
+        // ▶ file:////home/ubuntu/deploy/MR_v2sbm3c_blog/mreview/storage
+      
+        // JSP 인식되는 경로: http://localhost:9092/mreview/storage";
+        registry.addResourceHandler("/mreview/storage/**").addResourceLocations("file:///" +  Movie.getUploadDir());
+        
+        // JSP 인식되는 경로: http://localhost:9092/attachfile/storage";
+        // registry.addResourceHandler("/mreview/storage/**").addResourceLocations("file:///" +  Tool.getOSPath() + "/attachfile/storage/");
+        
+        // JSP 인식되는 경로: http://localhost:9092/member/storage";
+        // registry.addResourceHandler("/mreview/storage/**").addResourceLocations("file:///" +  Tool.getOSPath() + "/member/storage/");
+    }
+ 
+}
